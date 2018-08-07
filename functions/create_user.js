696a71c79c864b090f3996589b4ed16e0ecc5eb3
Adam Marczyk <adam.marczyk90@gmail.com>
2018-08-07 09:07:36 +0200

    add create user

--- functions/create_user.js
+++ functions/create_user.js
@@ -6,4 +6,10 @@ module.exports = function(req, res) {
   }
 
   const phone = String(req.body.phone).replace(/[^\d]/g, '');
+
+  admin
+    .auth()
+    .createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }));
 };
